1e2a79b01e8a3ff06774ec4a809a81a7dbee715d
absolve <wangding1992@126.com>
2025-03-13 14:33:42 +0800

    更新功能

--- godot3BattleCity/script/editmap.gd
+++ godot3BattleCity/script/editmap.gd
@@ -2,40 +2,59 @@ extends Node2D
 
 var tile=preload("res://scene/tile.tscn")
 
-var offset=Vector2(32,16)
+var offset=Vector2(32,16) #偏移
 var cellSize=16	#每个格子的大小是16px
 var mapRect  #地图大小
 var isPress=false #是否按下按键
-var itemType={'typeA':[{'x':0,'y':0,'type':Game.brickType.WALL}]
+var itemType={'del':[],
+			'typeA':[{'x':0,'y':0,'type':Game.brickType.WALL}]
 			,'typeB':[{'x':0,'y':0,'type':Game.brickType.STONE}],
 			'typeC':[{'x':0,'y':0,'type':Game.brickType.WATER}],
 			'typeD':[{'x':0,'y':0,'type':Game.brickType.BUSH}],
 			'typeE':[{'x':0,'y':0,'type':Game.brickType.ICE}],
 			'typeF':[{'x':0,'y':0,'type':Game.brickType.WALL},{'x':1,'y':0,'type':Game.brickType.WALL},
 			{'x':0,'y':1,'type':Game.brickType.WALL},{'x':1,'y':1,'type':Game.brickType.WALL}],
-			'typeG':[{'x':0,'y':0,'type':Game.brickType.WALL},{'x':0,'y':1,'type':Game.brickType.WALL}],
-			'typeH':[{'x':0,'y':0,'type':Game.brickType.STONE},{'x':1,'y':0,'type':Game.brickType.STONE},
+			'typeG':[{'x':0,'y':0,'type':Game.brickType.STONE},{'x':1,'y':0,'type':Game.brickType.STONE},
 			{'x':0,'y':1,'type':Game.brickType.STONE},{'x':1,'y':1,'type':Game.brickType.STONE}],
-			'typeJ':[{'x':0,'y':0,'type':Game.brickType.WATER},{'x':0,'y':1,'type':Game.brickType.WATER},
+			'typeH':[{'x':0,'y':0,'type':Game.brickType.WATER},{'x':0,'y':1,'type':Game.brickType.WATER},
 			{'x':1,'y':0,'type':Game.brickType.WATER},{'x':1,'y':1,'type':Game.brickType.WATER}],
 			'typeI':[{'x':0,'y':0,'type':Game.brickType.BUSH},{'x':0,'y':1,'type':Game.brickType.BUSH},
 			{'x':1,'y':0,'type':Game.brickType.BUSH},{'x':1,'y':1,'type':Game.brickType.BUSH}],
-			'typeL':[{'x':0,'y':0,'type':Game.brickType.ICE},{'x':0,'y':1,'type':Game.brickType.ICE},
+			'typeJ':[{'x':0,'y':0,'type':Game.brickType.ICE},{'x':0,'y':1,'type':Game.brickType.ICE},
 			{'x':1,'y':0,'type':Game.brickType.ICE},{'x':1,'y':1,'type':Game.brickType.ICE}],
 			}
+var itemMetadata=['del','typeA','typeB','typeC','typeD','typeE','typeF','typeG',
+					'typeH','typeI','typeJ']
+var itemImg=["res://sprite/del.png","res://sprite/brick.png","res://sprite/stone.png",
+			"res://sprite/water1.png","res://sprite/bush.png","res://sprite/ice.png",
+			"res://sprite/wall1.png","res://sprite/stone1.png","res://sprite/water4.png",
+			"res://sprite/bush1.png","res://sprite/ice1.png"]
 			
 #基地旁的方块
 var baseBrickPos=[Vector2(11,25),Vector2(11,24),Vector2(11,23),
 			Vector2(12,23),Vector2(13,23),Vector2(14,23),
 			Vector2(14,25),Vector2(14,24)]
 	
-var currentItem=''
+var currentItem=''  #当前选中的类型
 onready var childNode=$child
 onready var itemList=$Control/ItemList
+onready var fileDialog=$Control/FileDialog
+onready var loadDialog=$Control/loadDialog
 
 func _ready():
 	VisualServer.set_default_clear_color('#000')
 	mapRect =Rect2(offset,Vector2(cellSize*26,cellSize*26))
+	addBaseBrick()
+		
+	for i in itemImg:
+		var temp=load(i)
+		itemList.add_icon_item(temp)
+	for i in range(itemMetadata.size()):
+		itemList.set_item_metadata(i,itemMetadata[i])
+	currentItem='del'
+
+#添加基地周边的方块
+func addBaseBrick():
 	for i in baseBrickPos:
 		var temp=tile.instance()
 		temp.type=Game.brickType.WALL 
@@ -43,30 +62,6 @@ func _ready():
 		temp.position.y=i['y']*cellSize+cellSize/2+offset.y
 		temp.mapPos=Vector2(i['x'],i['y'])
 		childNode.add_child(temp)
-	var del=load("res://sprite/del.png")
-	var wall=load("res://sprite/brick.png")
-	var stone=load("res://sprite/stone.png")
-	var water=load("res://sprite/water1.png")
-	var bush=load("res://sprite/bush.png")
-	var ice=load("res://sprite/ice.png")
-	var wall1=load("res://sprite/wall1.png")
-	var stone1=load("res://sprite/stone1.png")
-	var water1=load("res://sprite/water4.png")
-	var bush1=load("res://sprite/bush1.png")
-	var ice1=load("res://sprite/ice1.png")
-	itemList.add_icon_item(del)	
-	itemList.add_icon_item(wall)	
-	itemList.add_icon_item(stone)
-	itemList.add_icon_item(water)
-	itemList.add_icon_item(bush)
-	itemList.add_icon_item(ice)
-	itemList.add_icon_item(wall1)
-	itemList.add_icon_item(stone1)	
-	itemList.add_icon_item(water1)		
-	itemList.add_icon_item(bush1)		
-	itemList.add_icon_item(ice1)		
-
-
 		
 #载入地图
 func loadMap(filePath:String):
@@ -84,6 +79,7 @@ func loadMap(filePath:String):
 				temp.position.x=i['x']*cellSize+cellSize/2+offset.x
 				temp.position.y=i['y']*cellSize+cellSize/2+offset.y
 				temp.type=i['type']
+				temp.mapPos=Vector2(i['x'],i['y'])
 				childNode.add_child(temp)
 	else:
 		print('文件不存在')			
@@ -91,15 +87,27 @@ func loadMap(filePath:String):
 
 #添加方块	
 func addItem(pos):
-	
-	pass
-
-#检查方块
-func checkItem(pos:Vector2):
-	var node=null
 	pos-=offset
 	var indexX=int(pos.x)/cellSize
 	var indexY=int(pos.y)/cellSize
+	var list=itemType[currentItem]
+	print(list)
+	#删除已有的方块
+	for i in list:
+		var node=checkItem(i.x+indexX,i.y+indexY)
+		if node:
+			node.queue_free()
+	for i in list:
+		var temp=tile.instance()
+		temp.position.x=(i['x']+indexX)*cellSize+cellSize/2+offset.x
+		temp.position.y=(i['y']+indexY)*cellSize+cellSize/2+offset.y
+		temp.type=i.type
+		temp.mapPos=Vector2(i['x']+indexX,i['y']+indexY)
+		childNode.add_child(temp)
+
+#检查方块
+func checkItem(indexX,indexY):
+	var node=null
 	for i in childNode.get_children():
 		if i.mapPos.x==indexX&&i.mapPos.y==indexY:
 			node=i
@@ -108,23 +116,48 @@ func checkItem(pos:Vector2):
 
 
 #删除方块
-func clearItem():
+func clearItem(pos:Vector2):
+	pos-=offset
+	var indexX=int(pos.x)/cellSize
+	var indexY=int(pos.y)/cellSize
 	
-	pass
+	for i in childNode.get_children():
+		if i.mapPos.x==indexX&&i.mapPos.y==indexY:
+			i.queue_free()
+
+#保存到文件
+func save2File(fileName):
+	var data={"name":'',"data":[],"base":[],"author":"battlecity",
+	"description":""}
+	for i in childNode.get_children():
+		var temp={'x':i.mapPos.x,'y':i.mapPos.y,'type':i.type}
+		data['data'].append(temp)
+	var file = File.new()
+	file.open(fileName, File.WRITE)
+	file.store_string(to_json(data))
+	file.close()
+
 
 func _input(event):
-	
+	if fileDialog.visible||loadDialog.visible:
+		return
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT &&  event.pressed:
 			isPress=true
 			if mapRect.has_point(get_global_mouse_position()):
-				
-				pass
+				if currentItem=='del':
+					clearItem(get_global_mouse_position())
+				else:
+					addItem(get_global_mouse_position())
 		elif !event.pressed:
 			isPress=false
 	elif event is InputEventMouseMotion:	#移动
 		if isPress:		
-			pass
+			if mapRect.has_point(get_global_mouse_position()):
+				if currentItem=='del':
+					clearItem(get_global_mouse_position())
+				else:
+					addItem(get_global_mouse_position())	
 
 
 func _draw():
@@ -136,4 +169,46 @@ func _draw():
 
 
 func _on_ItemList_item_selected(index):
-	pass # Replace with function body.
+	print(itemList.get_item_metadata(index))
+	currentItem=itemList.get_item_metadata(index)
+
+
+
+func _on_FileDialog_confirmed():
+	var path=fileDialog.current_dir	
+	print(path)
+	if fileDialog.current_file:
+		save2File(fileDialog.current_file)
+
+
+func _on_loadDialog_file_selected(path):
+	loadMap(path)
+
+
+func _on_loadDialog_confirmed():
+	var dir=loadDialog.current_dir
+	var file=loadDialog.current_file
+	if file:
+		loadMap(dir+"/"+file)
+
+
+func _on_load_pressed():
+	var baseDir=OS.get_executable_path().get_base_dir()
+	loadDialog.current_dir=baseDir
+	loadDialog.popup_centered()
+
+
+func _on_save_pressed():
+	var baseDir=OS.get_executable_path().get_base_dir()
+	fileDialog.current_dir=baseDir
+	fileDialog.current_file="2025.json"
+	fileDialog.popup_centered()
+
+
+func _on_clear_pressed():
+	for i in childNode.get_children():
+		i.queue_free()
+	addBaseBrick()
+
+func _on_return_pressed():
+	Game.changeScene("res://scene/welcome.tscn")
